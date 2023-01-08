d11cf3ee6ca60f9278f84671dda1c2f3ccb17d86
tom-urkin <tom.urkin@gmail.com>
2023-01-08 14:48:36 +0200

    Update source files and README
    include the asynchronous design

--- FIFO.sv
+++ FIFO.sv
@@ -1,40 +1,56 @@
 //Synchronous and Asynchronous FIFO memory
 
-module FIFO(clk,rst,data_in,FIFO_empty,FIFO_full,data_out,wr_en,rd_en,avail);
+module FIFO(wr_clk,wr_rst,data_in,wr_en,FIFO_full,avail, rd_clk,rd_rst,data_out,rd_en,FIFO_empty);
 
 //Parameters
 parameter DATA_WIDTH = 8;                                     //Word width - 8-bit word in deafualt settings
 parameter ADDR_WIDTH = 5;                                     //FIFO memory depth - 32 words in default settings
-parameter TYPE = 0;                                           //FIFO memory type: Synchronous ('0') or Asynchronous ('1')
-//Inputs
-input logic clk;                                              //FIFO memory inout clock
-input logic rst;                                              //Active high logic
-input logic [(DATA_WIDTH-1):0] data_in;                       //input data with DATA_WIDTH length
-input logic wr_en;                                            //Write request from external logic - writing to FIFO memory when logic high for one cycle
-input logic rd_en;                                            //Read request from external logic - reading from FIFO memory when logic high for one cycle
-//Outputs
-output logic FIFO_empty;                                      //Logic high when the FIFO memory is empty - do not read from memory
-output logic FIFO_full;                                       //Logic high when the FIFO memory is full - do not write to memory
-output logic [(DATA_WIDTH-1):0] data_out;                     //FIFO memory output
-output logic [ADDR_WIDTH:0] avail;								  //Number of available memory slots
-//Internal signals
-logic [ADDR_WIDTH:0] rptr;                                   //Read pointer
-logic [ADDR_WIDTH:0] wptr;                                   //Write pointer
-logic [DATA_WIDTH-1:0] mem [(2**ADDR_WIDTH-1):0];            //FIFO memory registers (32 8-bit words in defaults settings)
+parameter TYPE = 1;                                           //FIFO memory type: Synchronous ('0') or Asynchronous ('1')
 
+//Inputs
+input logic wr_clk;                                           //Write-side clock
+input logic wr_rst;                                           //Write-side asynchronous reset
+input logic [(DATA_WIDTH-1):0] data_in;                       //Input data to be written
+input logic wr_en;                                            //Write request from external logic
+
+input logic rd_clk;                                           //Read-side clock
+input logic rd_rst;                                           //Read-side asynchronous reset
+input logic rd_en;                                            //Read request from external logic
+
+//Outputs
+output logic FIFO_empty;                                      //Logic high when the FIFO memory is empty - calculated at the 'read' side
+output logic FIFO_full;                                       //Logic high when the FIFO memory is full - calculated at the 'write' side
+output logic [(DATA_WIDTH-1):0] data_out;                     //FIFO memory output
+output logic [ADDR_WIDTH:0] avail;                            //Number of available memory slots - calculated at the 'write' side
+
+//Internal signals
+logic [ADDR_WIDTH:0] rptr;                                    //Read pointer in the 'read' domain
+logic [ADDR_WIDTH:0] wptr;                                    //Write pointer in the 'write'domain
+logic [DATA_WIDTH-1:0] mem [(2**ADDR_WIDTH-1):0];             //FIFO memory registers (32 8-bit words in defaults settings)
+
+logic [ADDR_WIDTH:0] rgray;                                   //Gray-code equivalent of the read pointer
+logic [ADDR_WIDTH:0] wgray;                                   //Gray-code equivalent of the write pointer
+
+logic [ADDR_WIDTH:0] w_ff1_rgray;                             //Two-flop synchronization to capture the gray-coded read pointer
+logic [ADDR_WIDTH:0] w_ff2_rgray;                             //Two-flop synchronization to capture the gray-coded read pointer
+
+logic [ADDR_WIDTH:0] r_ff1_wgray;                             //Two-flop synchronization to capture the gray-coded write pointer
+logic [ADDR_WIDTH:0] r_ff2_wgray;                             //Two-flop synchronization to capture the gray-coded write pointer
+
+logic [ADDR_WIDTH:0] binary_w_ff2_rgray;
 
 integer i;                                                   //Used for reseting the FIFO memory
 
 //HDL code
 
 generate
-	if (TYPE==0)
+	if (TYPE==0)                                             //Synchronous FIFO memory
 		begin
-			always @(posedge clk or negedge rst)
-				if (!rst)                                                //Reseting the FIFO memory
+			always @(posedge wr_clk or negedge wr_rst)       //wr_clk and rd_clk are the same in synchronous omplementation
+				if (!wr_rst)                                                //Reseting the FIFO memory
 					begin
 						for (i=0; i<(2**ADDR_WIDTH); i=i+1)
-							mem[i]='d0;
+							mem[i]<=0;
 							wptr<=0;
 					end
 				else
@@ -44,8 +60,8 @@ generate
 						wptr<=wptr+1;
 					end
 
-		always @(posedge clk or negedge rst)
-			if (!rst)
+		always @(posedge wr_clk or negedge wr_rst)
+			if (!wr_rst)
 				begin
 					rptr<=0;
 					data_out<='d0;
@@ -59,7 +75,73 @@ generate
 
 		assign FIFO_empty = (wptr==rptr) ? 1'b1 : 1'b0;                                       //If the pointers are equal there is no new data to be read
 		assign FIFO_full = ({~wptr[ADDR_WIDTH],wptr[ADDR_WIDTH-1:0]}==rptr) ? 1'b1 : 1'b0;    //If the (ADDR_WIDTH-1) LSB bits are equal and the MSB is with reversed polarity, the FIFO memory is full
-		assign avail = 2**ADDR_WIDTH - (wptr-rptr);
+		assign avail = 2**ADDR_WIDTH - (wptr-rptr);                                           //Number of unwritten memory slots
 		end
+		
+	else                                                                                      //Asynchronous FIFO memory
+		begin
+			//Write-side logic
+			always @(posedge wr_clk or negedge wr_rst)
+				if (!wr_rst)
+					begin
+						for (i=0; i<(2**ADDR_WIDTH); i=i+1)
+						mem[i]<=0;
+						wptr<=0;
+					end
+				else if ((~FIFO_full)&&(wr_en))
+					begin
+						mem[wptr[ADDR_WIDTH-1:0]]<=data_in;
+						wptr<=wptr+1;
+					end
+					
+			assign wgray = wptr^(wptr>>1);                              //Converting write pointer to its gray-code equivalent
+			
+			always @(posedge wr_clk or negedge wr_rst)                  //Capture gray-coded read pointer
+				if (!wr_rst)
+					{w_ff2_rgray,w_ff1_rgray}<={{ADDR_WIDTH{1'b0}},{ADDR_WIDTH{1'b0}}};
+				else
+					{w_ff2_rgray,w_ff1_rgray}<={w_ff1_rgray,rgray};
+			
+			assign FIFO_full = (~wgray[ADDR_WIDTH-:2]==w_ff2_rgray[ADDR_WIDTH-:2])&&(wgray[ADDR_WIDTH-2:0]==w_ff2_rgray[ADDR_WIDTH-2:0]);																	
+			
+			
+			always @(*)	begin                                           //Convert from Gray to Binary
+			binary_w_ff2_rgray=0;
+			for (i=0; i<(ADDR_WIDTH+1); i=i+1) 
+				if (i==0)
+					binary_w_ff2_rgray[ADDR_WIDTH]=w_ff2_rgray[ADDR_WIDTH];
+				else
+					binary_w_ff2_rgray[ADDR_WIDTH-i]=binary_w_ff2_rgray[ADDR_WIDTH-i+1]^w_ff2_rgray[ADDR_WIDTH-i];
+			end
+				
+			assign avail=2**ADDR_WIDTH-(wptr-binary_w_ff2_rgray);       //Number of unwritten memory slots
+			
+			//Read-side logic
+			always @(posedge rd_clk or negedge rd_rst)
+				if (!rd_rst)
+					begin
+					rptr<=0;
+					data_out<=0;
+					end
+				else if ((rd_en)&&(!FIFO_empty))
+					begin
+						rptr<=rptr+1;
+						data_out<=mem[rptr[ADDR_WIDTH-1:0]];
+					end
+					
+			assign rgray = rptr^(rptr>>1);                              //Converting read pointer to its gray-code equivalent
+			
+			always @(posedge rd_clk or negedge rd_rst)                  //Capture gray-coded write pointer
+				if (!rd_rst)
+					{r_ff2_wgray,r_ff1_wgray}={{ADDR_WIDTH{1'b0}},{ADDR_WIDTH{1'b0}}};
+				else
+					{r_ff2_wgray,r_ff1_wgray}={r_ff1_wgray,wgray};
+					
+			assign FIFO_empty = (r_ff2_wgray==rgray);                   //FIFO-empty calculation
+			
+		end
+		
 endgenerate
+
+
 endmodule
--- FIFO_TB.sv
+++ FIFO_TB.sv
@@ -1,5 +1,5 @@
 `timescale 1ns/100ps
-//Following TB verifies the Synchronous_FIFO operation including reading, writing and FIFO full/empty conditions
+//Following TB verifies the Synchronous/Asynchronous FIFO memory operation including reading, writing and FIFO full/empty conditions
 module FIFO_TB();
 
 //Parameter declarations
@@ -7,57 +7,71 @@ module FIFO_TB();
 parameter DATA_WIDTH = 8;                        //Word width
 parameter ADDR_WIDTH = 5;                        //FIFO memory depth
 parameter SYNCHRONOUS = 0;
+parameter ASYNCHRONOUS = 1;
 parameter CLK_PERIOD = 20;
 //Internal signals declarations
-logic clk;
-logic rst;
+logic wr_clk;
+logic wr_rst;
 logic [DATA_WIDTH-1:0] data_in;
-logic [DATA_WIDTH-1:0] data_out;
-logic FIFO_empty;								//Memory empty indicator
-logic FIFO_full;								//Memory full indicator
 logic wr_en;									//FIFO memory write enable
+logic FIFO_full;								//Memory full indicator
+logic [ADDR_WIDTH:0] avail;
+
+logic rd_clk;
+logic rd_rst;
+logic [DATA_WIDTH-1:0] data_out;
 logic rd_en;									//FIFO memory read enable
+logic FIFO_empty;								//Memory empty indicator
+
 logic r_w;										//Randomize read/write command for test #3
-logic [ADDR_WIDTH-1:0] wptr;					//FIFO write pointer used in test #3
-logic [ADDR_WIDTH-1:0] rptr;					//FIFO read pointer used in test #3
+logic [ADDR_WIDTH-1:0] wptr_tst;					//FIFO write pointer used in test #3
+logic [ADDR_WIDTH-1:0] rptr_tst;					//FIFO read pointer used in test #3
 integer k;
 
+
 //FIFO memory module instantiation
-FIFO #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .TYPE(SYNCHRONOUS)) U1(
-                .clk(clk),
-                .rst(rst),
+FIFO #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .TYPE(ASYNCHRONOUS)) U1(
+                .wr_clk(wr_clk),
+                .wr_rst(wr_rst),
                 .data_in(data_in),
-                .data_out(data_out),
-                .FIFO_empty(FIFO_empty),
+				.wr_en(wr_en),
                 .FIFO_full(FIFO_full),
-                .wr_en(wr_en),
-                .rd_en(rd_en)
+				.avail(avail),
+				.rd_clk(rd_clk),
+				.rd_rst(rd_rst),
+	            .data_out(data_out),
+				.rd_en(rd_en),
+				.FIFO_empty(FIFO_empty)
                 );
 
 //Initial blocks
 initial
 begin
-    rst=1'b0;
-    clk=1'b0;
+    wr_rst=1'b0;
+	rd_rst=1'b0;
+    wr_clk=1'b0;
+	rd_clk=1'b0;
     rd_en=0;
     wr_en=0;
+	
 	r_w=0;
-	wptr=0;
-	rptr=0;
-    @(posedge clk)
-    rst=1'b1;
-    @(posedge clk)
+	wptr_tst=0;
+	rptr_tst=0;
+    @(posedge wr_clk)
+    wr_rst=1'b1;
+    @(posedge rd_clk)
+	rd_rst=1'b1;
 
     //----------------------------------------//
     //Test #1: Writing to the FIFO memory until full. Verify correctness of the writeen data and observe the FIFO_full signal
     for(k=0; k<(2**ADDR_WIDTH+5); k++)        //
         begin
         data_in= $random%8;                  //8-bit random number to be written to the FIFO memory
-        @(posedge clk)
+        @(posedge wr_clk)
         wr_en=1;                             //Enabling write operation
-        @(posedge clk)
+        @(posedge wr_clk)
         wr_en=0;
-        #1;
+        #1;									//Added delay to allow the FIFO block to execute before the comparison task 
         if (U1.mem[k] == data_in)
 			begin
             $display("Data written is %b data stored in memory is %b on iteration number %d- success",data_in,U1.mem[k],k);
@@ -80,9 +94,9 @@ begin
 
     for(k=0; k<(2**ADDR_WIDTH+5); k++)    //
         begin
-        @(posedge clk)
+        @(posedge rd_clk)
         rd_en=1;                         //Enabling read operation
-        @(posedge clk)
+        @(posedge rd_clk)
         rd_en=0;
         #1;
         if (U1.mem[k] == data_out)
@@ -106,59 +120,70 @@ begin
     //Test #3: Reading and writing from the FIFO memory for 10*FIFO depth times with randomized read/write operations
     for(k=0; k<(2**ADDR_WIDTH*10); k++)       
         begin
-		r_w= {$random}%2;                      //randomizing read/write operation. '0' for read and '1' for write operation.	
+		
+		r_w={$random}%2; //randomizing read/write operation. '0' for read and '1' for write operation.
+		
         data_in= $random%8;                    //8-bit random number to be written to the FIFO memory
+		wptr_tst=U1.wptr[ADDR_WIDTH-1:0];
+		rptr_tst= U1.rptr[ADDR_WIDTH-1:0];		
 		if (r_w==0)							   //Write operation
 			begin
-				wptr=U1.wptr[ADDR_WIDTH-1:0];
-				@(posedge clk)
-				wr_en=1;                     //Enabling write operation
-				@(posedge clk)
-				wr_en=0;
-				#1
 				if (FIFO_full==1)
 					begin
 					$display("\nThe FIFO memory is full - write operation was not completed on iteraion %d", k);
 					$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
 					$display("Remaning memory slots : %d", U1.avail);
+					@(posedge wr_clk);
 					end
-				else if (U1.mem[wptr] == data_in)
+				else 
 					begin
-					$display("\nData written is %b data stored in memory is %b on iteration number %d- success",data_in,U1.mem[wptr],k);
-					$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
-					$display("Remaning memory slots : %d", U1.avail);
-					end
-				else
-					begin
-						$display("Data written is %b data stored in memory is %b on iteration %d- write oepartion failed",data_in,U1.mem[wptr],k);
-						$finish;
+					@(posedge wr_clk)
+					wr_en=1;                     //Enabling write operation
+					@(posedge wr_clk)
+					wr_en=0;
+					#1										
+					if (U1.mem[wptr_tst] == data_in)
+						begin
+							$display("\nData written is %b data stored in memory is %b on iteration number %d- success",data_in,U1.mem[wptr_tst],k);
+							$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
+							$display("Remaning memory slots : %d", U1.avail);
+						end
+					else
+						begin
+							$display("Data written is %b data stored in memory is %b on iteration %d- write oepartion failed",data_in,U1.mem[wptr_tst],k);
+							$finish;
+						end
 					end
 			end
 		else
 			begin							//Read operation
-				rptr=U1.rptr[ADDR_WIDTH-1:0];
-				@(posedge clk)
-				rd_en=1;					
-				@(posedge clk)
-				rd_en=0;
-				#1;	
-				if (U1.mem[rptr] == data_out)
-					begin
-					$display("\nData read is %b data stored in memory is %b on iteration number %d- success",data_out,U1.mem[rptr],k);
-					$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
-					$display("Remaning memory slots : %d", U1.avail);
-					end
-				else if (FIFO_empty==1)
+				if (FIFO_empty==1)
 				begin
 					$display("\nThe FIFO memory is empty - read operation was not completed on iteraion %d", k);
 					$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
 					$display("Remaning memory slots : %d", U1.avail);
-				end
-				else
+					@(posedge rd_clk);					
+				end				
+				else 
 					begin
-						$display("Data read is %b data stored in memory is %b on iteration %d- read oepartion failed",data_out,U1.mem[rptr],k);
-						$finish;
-					end				
+					@(posedge rd_clk)
+					rd_en=1;					
+					@(posedge rd_clk)
+					rd_en=0;
+					#1;							
+						if (U1.mem[rptr_tst] == data_out)
+							begin
+							$display("\nData read is %b data stored in memory is %b on iteration number %d- success",data_out,U1.mem[rptr_tst],k);
+							$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
+							$display("Remaning memory slots : %d", U1.avail);
+							end
+						else
+							begin
+							$display("Data read is %b data stored in memory is %b on iteration %d- read oepartion failed",data_out,U1.mem[rptr_tst],k);
+							$display("The wptr value is %d and rptr is %d", U1.wptr, U1.rptr);
+							$finish;
+							end	
+					end
 			end
 
 
@@ -175,7 +200,8 @@ end
 always
 begin
 #(CLK_PERIOD/2);
-clk=~clk;
+wr_clk=~wr_clk;
+rd_clk=~rd_clk;
 end
 
 
